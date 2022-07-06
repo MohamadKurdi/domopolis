44d9fe6ce937697f167551b64b7cb09193238b50
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2022-07-06 21:05:44 +0300

    Removed test from maingun lib

--- catalog/controller/kp/mailgun.php
+++ catalog/controller/kp/mailgun.php
@@ -2,24 +2,6 @@
 	
 	class ControllerKPMailGun extends Controller {	
 
-		public function test(){
-
-
-
-			$mail = new Mail($this->registry);
-			$mail->setTo('v.zaichikov@gmail');
-			$mail->setFrom($this->config->get('config_email'))->setSender("Kitchen Profi");		
-			$mail->setSubject(html_entity_decode('test', ENT_QUOTES, 'UTF-8'))->setHTML('hello');
-			$mail->send();
-
-
-
-
-
-
-
-		}
-
 
 		private function validateAuthorization($data){	
 		
