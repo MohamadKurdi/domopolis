229838213f71b01460a6353908f6076b066ae2ee
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2022-08-30 15:20:19 +0300

    Fixed zero width and height resize

--- system/library/imageMagick.php
+++ system/library/imageMagick.php
@@ -7,26 +7,39 @@ class Image {
     private $bits;
     private $mime;
     private $hasAlpha;
+    private $log;
 
     public function __construct($file) {
         if (is_file($file)) {
-            $this->file     = $file;
+            
 
             try {  
 
-                $this->image    = new \Imagick($file);
-                $this->width    = $this->image->getImageWidth();
-                $this->height   = $this->image->getImageHeight();
-                $this->bits     = $this->image->getImageLength();
-                $this->mime     = $this->image->getFormat();
-                $this->hasAlpha = $this->image->getImageAlphaChannel();
+                $this->init($file);
                 
-            } catch (Exception $e) {
-                echo $e->getMessage();
+            } catch (\ImagickException $e) {
+                require_once(DIR_SYSTEM . 'library/log.php');
+                $this->log = new Log('imagick-error.txt');
+                $this->log->write(get_class($this) . ', ' .  $this->file . ': ' . $e->getMessage());
+
+                $this->log->debug($e->getMessage());
+
+               // $this->init(DIR_IMAGE . 'no_image.jpg');
             }
 
-        } else {           
         }
+    }
+
+    protected function init($file){
+
+        $this->file     = $file;
+        $this->image    = new \Imagick($file);
+        $this->width    = $this->image->getImageWidth();
+        $this->height   = $this->image->getImageHeight();
+        $this->bits     = $this->image->getImageLength();
+        $this->mime     = $this->image->getFormat();
+        $this->hasAlpha = $this->image->getImageAlphaChannel();
+
     }
 
     public static function cachedname($filename, $extension, $data = array()){
@@ -99,45 +112,64 @@ class Image {
      * @return void
      */
     public function resize($width = 0, $height = 0, $default = '') {
-        if (!$this->width || !$this->height) {
-            return;
-        }
 
-        switch ($default) {
-            case 'w':
-                $height = $width;
-                break;
-            case 'h':
-                $width = $height;
-                break;
-        }
-            
-        $this->image->thumbnailImage($width, $height, 1, 0);        
+        try {
 
-        $this->width    = $this->image->getImageWidth();
-        $this->height   = $this->image->getImageHeight();
-
-        if ($width == $height && $this->width != $this->height) {           
-            $squareImage = new Imagick();
-
-            if ($this->mime == 'image/png') {
-                $imagickPixel = new \ImagickPixel('transparent');
-            } else {
-                $imagickPixel = new \ImagickPixel('white');
+            if (!$width){
+                $width = $this->width;
             }
 
-            $squareImage->newImage($width, $height, $imagickPixel);          
+            if (!$width){
+                $height = $this->height;
+            }
+
+            if (!$this->width || !$this->height) {
+                return;
+            }
+
+            switch ($default) {
+                case 'w':
+                $height = $width;
+                break;
+                case 'h':
+                $width = $height;
+                break;
+            }
             
-            $x = (int)(($width - $this->width) / 2);
-            $y = (int)(($height - $this->height) / 2);
+            $this->image->thumbnailImage($width, $height, 1, 0);        
 
-            $squareImage->compositeImage($this->image, \Imagick::COMPOSITE_COPYOPACITY , $x, $y);
-            $squareImage->compositeImage($this->image, \Imagick::COMPOSITE_DEFAULT , $x, $y);
-            $this->image = $squareImage;
-           
+            $this->width    = $this->image->getImageWidth();
+            $this->height   = $this->image->getImageHeight();
 
-            $this->width = $this->image->getImageWidth();
-            $this->height = $this->image->getImageHeight();
+            if ($width == $height && $this->width != $this->height) {           
+                $squareImage = new Imagick();
+
+                if ($this->mime == 'image/png') {
+                    $imagickPixel = new \ImagickPixel('transparent');
+                } else {
+                    $imagickPixel = new \ImagickPixel('white');
+                }
+
+                $squareImage->newImage($width, $height, $imagickPixel);          
+
+                $x = (int)(($width - $this->width) / 2);
+                $y = (int)(($height - $this->height) / 2);
+
+                $squareImage->compositeImage($this->image, \Imagick::COMPOSITE_COPYOPACITY , $x, $y);
+                $squareImage->compositeImage($this->image, \Imagick::COMPOSITE_DEFAULT , $x, $y);
+                $this->image = $squareImage;
+
+
+                $this->width = $this->image->getImageWidth();
+                $this->height = $this->image->getImageHeight();
+            }
+
+        } catch (\ImagickException $e) {
+            require_once(DIR_SYSTEM . 'library/log.php');
+                $this->log = new Log('imagick-error.txt');
+                $this->log->write(get_class($this) . ', ' . $this->file . ': ' . $e->getMessage());
+
+            $this->log->debug($e->getMessage());
         }
     }
 
@@ -153,39 +185,39 @@ class Image {
 
         switch ($position) {
             case 'overlay':
-                for ($width = 0; $width < $this->width; $width += $watermark->getImageWidth()) {
-                    for ($height = 0; $height < $this->height; $height += $watermark->getImageHeight()) {
-                        $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $width, $height);
-                    }
+            for ($width = 0; $width < $this->width; $width += $watermark->getImageWidth()) {
+                for ($height = 0; $height < $this->height; $height += $watermark->getImageHeight()) {
+                    $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $width, $height);
                 }
-                break;
+            }
+            break;
             case 'topleft':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, 0);
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, 0);
+            break;
             case 'topcenter':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, intval(($this->width - $watermark->getImageWidth()) / 2), 0);
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, intval(($this->width - $watermark->getImageWidth()) / 2), 0);
+            break;
             case 'topright':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $this->width - $watermark->getImageWidth(), 0);
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $this->width - $watermark->getImageWidth(), 0);
+            break;
             case 'middleleft':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, intval(($this->height - $watermark->getImageHeight()) / 2));
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, intval(($this->height - $watermark->getImageHeight()) / 2));
+            break;
             case 'middlecenter':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, intval(($this->width - $watermark->getImageWidth()) / 2), intval(($this->height - $watermark->getImageHeight()) / 2));
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, intval(($this->width - $watermark->getImageWidth()) / 2), intval(($this->height - $watermark->getImageHeight()) / 2));
+            break;
             case 'middleright':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $this->width - $watermark->getImageWidth(), intval(($this->height - $watermark->getImageHeight()) / 2));
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $this->width - $watermark->getImageWidth(), intval(($this->height - $watermark->getImageHeight()) / 2));
+            break;
             case 'bottomleft':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, $this->height - $watermark->getImageHeight());
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, $this->height - $watermark->getImageHeight());
+            break;
             case 'bottomcenter':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, intval(($this->width - $watermark->getImageWidth()) / 2), $this->height - $watermark->getImageHeight());
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, intval(($this->width - $watermark->getImageWidth()) / 2), $this->height - $watermark->getImageHeight());
+            break;
             case 'bottomright':
-                $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $this->width - $watermark->getImageWidth(), $this->height - $watermark->getImageHeight());
-                break;
+            $this->image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $this->width - $watermark->getImageWidth(), $this->height - $watermark->getImageHeight());
+            break;
         }
     }
 
@@ -197,12 +229,12 @@ class Image {
      *
      * @return void
      */
-    public function crop($top_x, $top_y, $bottom_x, $bottom_y) {
-        $this->image->cropImage($bottom_x - $top_x, $bottom_y - $top_y, $top_x, $top_y);
+   public function crop($top_x, $top_y, $bottom_x, $bottom_y) {
+    $this->image->cropImage($bottom_x - $top_x, $bottom_y - $top_y, $top_x, $top_y);
 
-        $this->width = $this->image->getImageWidth();
-        $this->height = $this->image->getImageHeight();
-    }
+    $this->width = $this->image->getImageWidth();
+    $this->height = $this->image->getImageHeight();
+}
 
     /**
      * @param int $degree
