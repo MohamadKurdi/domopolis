0dc385b5f4caee82f68889ab675968bd634c3f4d
Victor <v.zaichikov@gmail.com>
2022-09-28 18:26:37 +0300

    Moved forming of query string to registry to use in models

--- system/engine/registry.php
+++ system/engine/registry.php
@@ -13,4 +13,21 @@ final class Registry {
 	public function has($key) {
 		return isset($this->data[$key]);
 	}
+
+	public function createCacheQueryString($method, $setting = [], $options = []){
+		$md5 = md5(serialize($setting) . serialize($options));
+
+		return  
+			$method . 
+			$this->get('config')->get('config_store_id') . 
+			$this->get('config')->get('config_language_id') . 
+			$this->get('customer_group_id') . 
+			$this->get('currency')->getId() . 
+			(int)ADD_METRICS_TO_FRONT . 
+			(int)WEBPACCEPTABLE . 
+			(int)AVIFACCEPTABLE . 
+			(int)IS_MOBILE_SESSION . 
+			(int)IS_TABLET_SESSION . 
+			$md5;
+	}
 }
