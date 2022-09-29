b510a1de65e1837827aa3b82aac42037dafce2aa
Victor <v.zaichikov@gmail.com>
2022-09-29 16:11:29 +0300

    Some minor changes in utf helper

--- system/helper/utf8.php
+++ system/helper/utf8.php
@@ -74,7 +74,7 @@ function prepareEOLArray($string){
 
 	return $result;
 }
-	
+
 function simple_translit($string){
 	$from = array('а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я','А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я','і','є');
 	$to = array('a','b','v','g','d','e','yo','zh','z','i','j','k','l','m','n','o','p','r','s','t','u','f','h','ts','ch','sh','sch','','y','','e','yu','ya','A','B','V','G','D','E','YO','ZH','Z','I','J','K','L','M','N','O','P','R','S','T','U','F','KH','TS','CH','SH','SCH','','Y','','E','YU','YA','i','ie');
@@ -124,6 +124,18 @@ function simple_rms($st){
 function simple_normalize($st){
 	return strtolower(rms(translit($st)));
 } 
+
+function strpos_offset($needle, $haystack, $occurrence) {			
+	$arr = explode($needle, $haystack);			
+	switch($occurrence) {
+		case $occurrence == 0:
+		return false;
+		case $occurrence > max(array_keys($arr)):
+		return false;
+		default:
+		return strlen(implode($needle, array_slice($arr, 0, $occurrence)));
+	}
+}
 
 function bool_real_stripos($haystack, $needle){			
 	return !(stripos($haystack, $needle) === false);			
