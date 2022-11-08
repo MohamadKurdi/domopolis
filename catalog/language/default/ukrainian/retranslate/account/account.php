a990209681be6ed516541b7ea3a4803eccc0e857
Victor <v.zaichikov@gmail.com>
2022-11-08 16:34:42 +0200

    Changed some language strings in ua-uk

--- catalog/language/default/ukrainian/retranslate/account/account.php
+++ catalog/language/default/ukrainian/retranslate/account/account.php
@@ -4,9 +4,9 @@ $_['text_retranslate_account_available_points'] = 'доступні бонуси
 
 $_['text_retranslate_account_main_line_1'] = 'З нами Ви завжди:';
 $_['text_retranslate_account_main_line_2'] = 'знайдете саме те що потрібно в зручному каталозі товарів';
-$_['text_retranslate_account_main_line_3'] = 'будете в курсі самих вигідних пропозицій завдяки розділах <a href="%s" title="Акціі"> Акції </a> і <a href="%s" title="Розпродаж" > Розпродаж</a>. ';
+$_['text_retranslate_account_main_line_3'] = 'будете в курсі самих вигідних пропозицій завдяки розділам <a href="%s" title="Акціі"> Акції </a> і <a href="%s" title="Розпродаж" > Розпродаж</a>. ';
 
 
 $_['text_retranslate_account_main_line_4'] = 'Привіт, %s! Ви в особистому кабінеті покупця';
 $_['text_retranslate_account_main_line_4_noname'] = 'Привіт! Ви в особистому кабінеті покупця';
-$_['text_retranslate_account_main_line_5'] = 'Тут знайдете всю інформацію про ваших покупках і замовленнях, зможете змінити особисті дані і купувати вигідніше з бонусами';
+$_['text_retranslate_account_main_line_5'] = 'Тут знайдете всю інформацію про ваші покупки і замовлення, зможете змінити особисті дані і купувати вигідніше з бонусами';
