d9395bfa28d219b9afe9cb568f54f5451d292163
Victor <v.zaichikov@gmail.com>
2022-11-17 14:53:03 +0200

    Added ability to use primary image source for video in catalog

--- catalog/model/tool/video.php
+++ catalog/model/tool/video.php
@@ -4,12 +4,16 @@ class ModelToolVideo extends Model {
 
 
 	public function getPath($video){
-			if (!file_exists(DIR_IMAGE . $video)){
+			$DIR_IMAGE_NAME = DIR_IMAGE_NAME;
+			if (file_exists(DIR_IMAGE . $video) && is_file(DIR_IMAGE . $video)){
+				//do nothing
+			} elseif (defined('DIR_IMAGE_MAIN') && file_exists(DIR_IMAGE_MAIN . $video) && is_file(DIR_IMAGE_MAIN . $video)){
+				$DIR_IMAGE_NAME = DIR_IMAGE_NAME_MAIN;
+			} else {
 				return '';
 			}
 
 			$videoServer = $this->config->get('config_ssl');	
-
 
 			if ($this->config->get('config_img_ssl')){					
 				$videoServer = $this->config->get('config_img_ssl');
@@ -24,6 +28,6 @@ class ModelToolVideo extends Model {
 				$videoServer = str_replace('{N}', ($first_symbol % ($this->config->get('config_img_server_count')+1)), $this->config->get('config_img_ssls')); 
 			}
 
-			return $videoServer . DIR_IMAGE_NAME . $video;			
+			return $videoServer . $DIR_IMAGE_NAME . $video;			
 		}
 }
