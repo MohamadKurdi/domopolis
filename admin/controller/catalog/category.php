84f22720c4df3f3605e776e0750268efaa8c73d8
Victor <v.zaichikov@gmail.com>
2022-12-26 14:19:09 +0200

    Added autocomplete for final categories

--- admin/controller/catalog/category.php
+++ admin/controller/catalog/category.php
@@ -1159,6 +1159,40 @@ class ControllerCatalogCategory extends Controller {
 		$this->response->setOutput(json_encode($json));
 	}
 
+	public function autocomplete_final() {
+		$json = array();
+
+		if (isset($this->request->get['filter_name'])) {
+			$this->load->model('catalog/category');
+
+			$data = array(
+				'filter_name' => $this->request->get['filter_name'],
+				'filter_final' => true,
+				'start'       => 0,
+				'limit'       => 20
+			);
+
+			$results = $this->model_catalog_category->getCategories($data);
+
+			foreach ($results as $result) {
+				$json[] = array(
+					'category_id' => $result['category_id'], 
+					'name'        => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))
+				);
+			}		
+		}
+
+		$sort_order = array();
+
+		foreach ($json as $key => $value) {
+			$sort_order[$key] = $value['name'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $json);
+
+		$this->response->setOutput(json_encode($json));
+	}	
+
 	public function autocomplete() {
 		$json = array();
 
