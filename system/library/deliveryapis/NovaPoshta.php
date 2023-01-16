19eca643ec44d25e37ca4213e3e30d13c793489c
Victor <v.zaichikov@gmail.com>
2023-01-16 17:00:23 +0200

    Refactored CourierServices NovaPoshta

--- system/library/deliveryapis/NovaPoshta.php
+++ system/library/deliveryapis/NovaPoshta.php
@@ -14,35 +14,22 @@
 		public function __construct($registry) {
 			
 				$this->registry = $registry;
-				$this->config = $this->registry->get('config');
-				$this->db = $this->registry->get('db');
+				$this->config 	= $this->registry->get('config');
+				$this->db 		= $this->registry->get('db');
 				
 				$this->apiKey = $this->config->get('config_novaposhta_api_key');
 				$this->defaultCityGuid = $this->config->get('config_novaposhta_default_city_guid');
 		}
 		
-		private function validateResult($result){
-			$tmp = $result;
-			
-			$result = json_decode($result, true);
-			
-			if (!$result || !is_array($result)){
-				print_r($tmp);
-				throw new Exception('Нечитаемый JSON');
+		private function validateResult($result){			
+			if (!$result || mb_strlen($result) < 5 || !($json = json_decode($result, true)) || empty($json['success']) || !$json['success']){				
+				throw new Exception('Erroneous response from NovaPoshta API, possibly wrong invoice number! Or problems with the API ' . serialize($result));
 			}
 			
-			if ($result['success'] === 'false'){
-				print_r($tmp);
-				throw new Exception('Ошибка обработки ' . serialize($result));
-			}
-			
-			return $result;
-			
-		}
+			return $json;			
+		}		
 		
-		
-		private function doRequest($data){
-			
+		private function doRequest($data){			
 			$json = array(			
 			'apiKey' 			=> $this->apiKey			
 			);
@@ -50,26 +37,35 @@
 			foreach ($data as $key => $value){
 				$json[$key] = $value;
 			}
-			
-			
-			$json = json_encode($json);
-			
+
 			echoLine('[NP] Запрос ' . $data['modelName'] . '/' . $data['calledMethod']);
 			
 			$curl = curl_init($this->host);
-			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8', 'Content-Length: '.strlen($json)));
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($json));
+
+			$headers[] = 'Host: api.novaposhta.ua';
+			$headers[] = 'Origin: https://tracking.novaposhta.ua';
+			$headers[] = 'Referer: https://tracking.novaposhta.ua/';
+			$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0';
+			$headers[] = 'Accept: application/json;q=0.9,*/*;q=0.8';
+			$headers[] = 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3';
+			$headers[] = 'Encoding: gzip, deflate, br';
+			$headers[] = 'Cookie: forceDesktop=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT; Max-Age=0; path=/';
+			$headers[] = 'Connection: keep-alive';
+			$headers[] = 'Cache-Control: max-age=0';
+			$headers[] = 'Content-Type: application/json; charset=utf-8';
+			$headers[] = 'Content-Length: '. mb_strlen(json_encode($json));
+
+			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 100);	
-			curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-			//	curl_setopt($curl, CURLOPT_VERBOSE, true);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 500);			
 			
-			$result = curl_exec($curl);
-			
+			$result = curl_exec($curl);			
 			curl_close($curl);
 			
-			try{
-				
+			try{				
 				$result = $this->validateResult($result);
 				return $result;
 				
@@ -77,7 +73,139 @@
 				
 				echoLine('ОШИБКА ОБРАБОТКИ');
 				echoLine($e->getMessage());
+				return false;
 			}	
+		}
+
+
+		public function trackAndReturn($tracking_code, $phone = ''){
+
+		}
+
+		public function trackAndFormat($tracking_code, $phone = ''){
+			$data = [			
+				"language"  		=> "uk",
+				"system" 			=> "Tracking", 	
+				"modelName"			=> "InternetDocument",
+				"calledMethod"  	=> "getDocumentsEWMovement",
+				"methodProperties" 	=> [ 
+					"NewFormat" => 1,
+					"Number"   	=> $tracking_code
+				]
+			];
+
+			if ($info = $this->doRequest($data)){
+				return $this->prettyFormatTrackingInfo($info);
+			}
+
+			return false;
+		}
+
+		public function prettyFormatTrackingInfo($json){
+			$status = '<b>' . $json['data'][0]['status']['description'] . '</b>, ' . $json['data'][0]['status']['addressDescription'] . '';
+			$status .= '<br />';
+			$status .= '<br />';
+			$status .= "<table class='list'>";
+
+			if (!empty($json['data'][0]['movement']['passed'])){
+				$status .= '<tr><td colspan="4" style="padding:5px 0px;"><b>Виконано:</b></td></tr>';
+			}
+
+			foreach ($json['data'][0]['movement']['passed'] as $movement){
+				if (!empty($movement['Date'])){
+					$time = $movement['Date'];
+				}
+				
+				if (!empty($movement['ArrivalTime'])){
+					$time = $movement['ArrivalTime'];
+				}
+
+				if (!empty($movement['EventDescription'])){
+					$status .= '<tr>';
+					$status .= '<td class="left">';
+					$status .= '<i class="fa fa-check"></i>';
+					$status .= '</td>';
+					$status .= '<td class="left">';
+					$status .= $movement['EventDescription'];
+					$status .= '</td>';
+					$status .= '<td class="left">';
+					$status .= $movement['WarehouseDescription'];
+					$status .= '</td>';
+					$status .= '<td class="left">';
+					$status .= date('d.m.Y H:i', strtotime($time));
+					$status .= '</td>';
+					$status .= '</tr>';
+				}
+			}
+
+			if (!empty($json['data'][0]['movement']['now'])){
+				$status .= '</table><table class="list">';
+				$status .= '<tr><td colspan="4" style="padding:5px 0px;"><b>Поточний стан:</b></td></tr>';
+			}
+
+			foreach ($json['data'][0]['movement']['now'] as $movement){
+				if (!empty($movement['Date'])){
+					$time = $movement['Date'];
+				}
+				
+				if (!empty($movement['ArrivalTime'])){
+					$time = $movement['ArrivalTime'];
+				}
+
+				if (!empty($movement['EventDescription'])){
+					$status .= '<tr>';
+					$status .= '<td class="left">';
+					$status .= '<i class="fa fa-check-circle"></i>';
+					$status .= '</td>';
+					$status .= '<td class="left"><b>';
+					$status .= $movement['EventDescription'];
+					$status .= '</b></td>';
+					$status .= '<td class="left"><b>';
+					$status .= $movement['WarehouseDescription'];
+					$status .= '</b></td>';
+					$status .= '<td class="left"><b>';
+					$status .= date('d.m.Y H:i', strtotime($time));
+					$status .= '</b></td>';
+					$status .= '</tr>';
+				}
+			}
+
+			if (!empty($json['data'][0]['movement']['future'])){
+				$status .= '</table><table class="list">';
+				$status .= '<tr><td colspan="4" style="padding:5px 0px;"><b>Доктор Стрендж передбачує:</b></td></tr>';
+			}
+
+			foreach ($json['data'][0]['movement']['future'] as $movement){
+				if (!empty($movement['Date'])){
+					$time = $movement['Date'];
+				}
+				
+				if (!empty($movement['ArrivalTime'])){
+					$time = $movement['ArrivalTime'];
+				}
+
+				if (!empty($movement['EventDescription'])){
+					$status .= '<tr>';
+					$status .= '<td class="left">';
+					$status .= '<i class="fa fa-spinner"></i>';
+					$status .= '</td>';
+					$status .= '<td class="left">';
+					$status .= $movement['EventDescription'];
+					$status .= '</td>';
+					$status .= '<td class="left">';
+					$status .= $movement['WarehouseDescription'];
+					$status .= '</td>';
+					$status .= '<td class="left">';
+					$status .= date('d.m.Y H:i', strtotime($time));
+					$status .= '</td>';
+					$status .= '</tr>';
+				}
+			}
+
+			$status .= '</table>';
+
+
+			return $status;	
 		}
 		
 		public function getDeliveryPrice(){
@@ -109,8 +237,7 @@
 				}
 			}
 			
-			return false;
-			
+			return false;			
 		}
 		
 		public function checkStatus(){
@@ -137,29 +264,29 @@
 		
 		public function updateZones(){
 			$data = array(
-			'modelName' 		=> 'Address',
-			'calledMethod' 		=> 'getAreas',		
+				'modelName' 		=> 'Address',
+				'calledMethod' 		=> 'getAreas',		
 			);
 			
 			
 			$result = $this->doRequest($data);
 			
 			foreach ($result['data'] as $line){
-			
-			echoLine('[NP] Область ' . $line['Description']);
-			
-			$this->db->query("INSERT INTO novaposhta_zones SET 
-			Ref = '" . $this->db->escape($line['Ref']) . "',
-			Description = '" . $this->db->escape($line['Description']) . "',
-			DescriptionRu = '" . $this->db->escape($line['DescriptionRu']) . "',
-			AreasCenter = '" . $this->db->escape($line['AreasCenter']) . "'
-			ON DUPLICATE KEY UPDATE
-			Description = '" . $this->db->escape($line['Description']) . "',
-			DescriptionRu = '" . $this->db->escape($line['DescriptionRu']) . "',
-			AreasCenter = '" . $this->db->escape($line['AreasCenter']) . "'");
-		}
-		
-		return $this;
+
+				echoLine('[NP] Область ' . $line['Description']);
+
+				$this->db->query("INSERT INTO novaposhta_zones SET 
+					Ref = '" . $this->db->escape($line['Ref']) . "',
+					Description = '" . $this->db->escape($line['Description']) . "',
+					DescriptionRu = '" . $this->db->escape($line['DescriptionRu']) . "',
+					AreasCenter = '" . $this->db->escape($line['AreasCenter']) . "'
+					ON DUPLICATE KEY UPDATE
+					Description = '" . $this->db->escape($line['Description']) . "',
+					DescriptionRu = '" . $this->db->escape($line['DescriptionRu']) . "',
+					AreasCenter = '" . $this->db->escape($line['AreasCenter']) . "'");
+			}
+
+			return $this;
 		}
 		
 		public function updateCities(){
@@ -288,8 +415,7 @@
 			
 			return $this;
 		}
-		
-		
+				
 		public function updateWareHouses(){
 			
 			$data = array(
@@ -376,8 +502,7 @@
 			//Обновление количества отделений
 			$this->db->query("UPDATE novaposhta_cities_ww SET WarehouseCount = (SELECT COUNT(Ref) FROM novaposhta_warehouses WHERE CityRef = novaposhta_cities_ww.Ref)");
 			
-			return $this;
-			
+			return $this;			
 		}
 		
 		public function getRealTimeStreets($CityRef){
