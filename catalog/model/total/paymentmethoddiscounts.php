1140a660e771508b2c0d0061880cd6c581a06100
Victor <v.zaichikov@gmail.com>
2023-01-17 19:38:29 +0200

    Some modifications in Payment Method Discounts

--- catalog/model/total/paymentmethoddiscounts.php
+++ catalog/model/total/paymentmethoddiscounts.php
@@ -1,53 +1,53 @@
 <?php
-	class ModelTotalPaymentMethodDiscounts extends Model {
-		public function getTotal(&$total_data, &$total, &$taxes) {			
-			
-			if(isset($this->session->data['payment_method'])){
-				
-				$discounts = $this->config->get('paymentmethoddiscounts_discount');
-				
-				$payment_method='';
-				if (isset($this->session->data['payment_method'])) {
-					$payment_method = $this->session->data['payment_method']['title'];
-				}
-				
-				if (isset($this->session->data['payment_secondary_method'])) {
-					$payment_method = $this->session->data['payment_secondary_method']['title'];
-				}
-				
-				$sub_total = $this->cart->getSubTotal();
-				$sub_total_national = $this->cart->getSubTotalInNationalCurrency();
-				
-				foreach ($discounts as $discount){
+class ModelTotalPaymentMethodDiscounts extends Model {
+	public function getTotal(&$total_data, &$total, &$taxes) {			
 
-					if ($discount['deliveries'] && $exploded = explode(PHP_EOL, $discount['deliveries'])){
-						
-						$status = false;
-						foreach ($exploded as $line){
-							if (trim($line) == $this->session->data['shipping_method']['code']){
-								$status = true;
-								break;
-							}
+		if(isset($this->session->data['payment_method'])){
+
+			$discounts = $this->config->get('paymentmethoddiscounts_discount');
+
+			$payment_method='';
+			if (isset($this->session->data['payment_method'])) {
+				$payment_method = $this->session->data['payment_method']['title'];
+			}
+
+			if (isset($this->session->data['payment_secondary_method'])) {
+				$payment_method = $this->session->data['payment_secondary_method']['title'];
+			}
+
+			$sub_total = $this->cart->getSubTotal();
+			$sub_total_national = $this->cart->getSubTotalInNationalCurrency();
+
+			foreach ($discounts as $discount){
+				$status = true;
+
+				if ($discount['deliveries'] && $exploded = explode(PHP_EOL, $discount['deliveries'])){
+					$status = false;
+					foreach ($exploded as $line){
+						if (trim($line) == $this->session->data['shipping_method']['code']){
+							$status = true;
+							break;
 						}
 					}
-										
-					if ($status && $discount['paymentmethod'] == $this->session->data['payment_method']['code'] || (isset($this->session->data['payment_secondary_method']) && $discount['paymentmethod'] == $this->session->data['payment_secondary_method']['code'])) {
-						$this->load->language('total/paymentmethoddiscounts');
-						
-						$number = 0;
-						
-						if($discount['znak']){
-							if($discount['mode']) {
+				}
+
+				if ($status && $discount['paymentmethod'] == $this->session->data['payment_method']['code'] || (isset($this->session->data['payment_secondary_method']) && $discount['paymentmethod'] == $this->session->data['payment_secondary_method']['code'])) {
+					$this->load->language('total/paymentmethoddiscounts');
+
+					$number = 0;
+
+					if($discount['znak']){
+						if($discount['mode']) {
 								$number = -$sub_total*$discount['number']/100; // -%
 								$number_national = -$sub_total_national*$discount['number']/100; // -%
-								} else {
+							} else {
 								$number = -$discount['number']; // -ed
 							}
-							}else{
+						}else{
 							if($discount['mode']) { 
 								$number =  $sub_total*$discount['number']/100; // +%
 								$number_national = $sub_total_national*$discount['number']/100; // -%
-								} else { 
+							} else { 
 								$number =  $discount['number']; 
 							}
 						}
@@ -59,52 +59,22 @@
 						if (isset($this->session->data['coupon']) AND mb_strlen($this->session->data['coupon'])>0){
 							$coupon_exists = true;
 						}
-						
-						$disable_discount = false;				
-						//если нет дисконтной карты или это не скидка
-						if ((bool)$this->customer->getDiscountCard() && $number_national < 0){
-							$disable_discount = true;
-						}
-						
-						//нет дисконтной карты, но пользователь залогинен и у него есть скидка - пиздец блять, дичь какая-то)
-						if (!$this->customer->getDiscountCard() && $this->customer->isLogged() && $number_national < 0){
-							$this->load->model('total/shoputils_cumulative_discounts');
-							
-							//проверим пересчитанный процент скидки
-							if ($this->customer->getDiscountPercent()){
-								$disable_discount = true;
-							}
-							
-							if (!$disable_discount && $this->model_total_shoputils_cumulative_discounts->getLoggedCustomerDiscount()){
-								$disable_discount = true;
-							}						
-						}
-						
+												
 						//если есть купон и это скидка то не применять
-						if (!$disable_discount && $coupon_exists && $number_national < 0){
-							$disable_discount = true;			
-						}
-						
-						//ВТОРАЯ ЛОГИКА, ПОХУЙ НА ПРЕДЫДУЩУЮ
-						$disable_discount = false;		
 						if ($coupon_exists && $number_national < 0){
-							$disable_discount = true;			
-						}
+							$status = false;			
+						}						
 						
-						
-						if (!$disable_discount){
-							
+						if ($status){							
 							$total_data[] = array( 
-							'code'       => 'paymentmethoddiscounts',
-							'title'      => $text,
-							'sub_text' 	 => '',
-							'text'       => $this->currency->format($number_national, $this->config->get('config_regional_currency'), '1'),
-							'value_national'   => $this->currency->convert($number_national, $this->config->get('config_regional_currency'), $this->config->get('config_regional_currency')),
-							'value'      => $number,
-							'sort_order' => $this->config->get('paymentmethoddiscounts_sort_order')
+								'code'       		=> 'paymentmethoddiscounts',
+								'title'      		=> $text,
+								'sub_text' 	 		=> '',
+								'text'       		=> $this->currency->format($number_national, $this->config->get('config_regional_currency'), '1'),
+								'value_national'   	=> $this->currency->convert($number_national, $this->config->get('config_regional_currency'), $this->config->get('config_regional_currency')),
+								'value'      		=> $number,
+								'sort_order' 		=> $this->config->get('paymentmethoddiscounts_sort_order')
 							);
-							
-							
 							
 							if ($discount['tax_class_id']) {
 								$tax_rates = $this->tax->getRates($number, $discount['tax_class_id']);
@@ -112,7 +82,7 @@
 								foreach ($tax_rates as $tax_rate) {
 									if (!isset($taxes[$tax_rate['tax_rate_id']])) {
 										$taxes[$tax_rate['tax_rate_id']] = $tax_rate['amount'];
-										} else {
+									} else {
 										$taxes[$tax_rate['tax_rate_id']] += $tax_rate['amount'];
 									}
 								}
