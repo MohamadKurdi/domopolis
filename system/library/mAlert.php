be50e7da9d521044ffda257deb32e51b860d8722
Victor <v.zaichikov@gmail.com>
2023-02-11 13:17:32 +0200

    Some changes in mAlert lib

--- system/library/mAlert.php
+++ system/library/mAlert.php
@@ -6,12 +6,11 @@ class mAlert {
 	//alert stucture: type:text:entity_type:entity_id
 	//example: sales:information:'Оформлен новый заказ':order:11816
 	//example: 
-	
-	
+		
 	public function __construct($registry) {
-		$this->cache= $registry->get('cache');
-		$this->db = $registry->get('db');		
-		$this->prefix = ALERT_QUEUE;
+		$this->cache 	= $registry->get('cache');
+		$this->db 		= $registry->get('db');		
+		$this->prefix 	= ALERT_QUEUE;
 		
 		if (!($this->queue = $this->cache->get($this->prefix . '_queue'))){
 			
@@ -25,23 +24,17 @@ class mAlert {
 			
 			$query = $this->db->query("SELECT GROUP_CONCAT(u.user_id SEPARATOR ':') as user_list FROM user u WHERE u.status=1 GROUP BY u.status");
 			$this->queue['everyone'] = $query->row['user_list'];		
-			
-			
-			$this->cache->set($this->prefix . '_queue', $this->queue);
-			
+						
+			$this->cache->set($this->prefix . '_queue', $this->queue);			
 		}
 	}
 	
 	
 	public function insertAlertForGroup($for_who, $data = array()){
-		
-		//pushing alert for every member of queue
 		if (isset($this->queue[$for_who])){
 		
 			foreach ($this->queue[$for_who] as $user_id) {
-				if ($user_id != 16 && $user_id != 17) {
-					$this->cache->addtolist($this->prefix . '_user' . (int)$user_id, serialize($data), 600);								
-				}
+				$this->cache->addtolist($this->prefix . '_user' . (int)$user_id, serialize($data), 600);								
 			}
 			
 			return true;
@@ -50,29 +43,11 @@ class mAlert {
 		return false;						
 	}
 	
-	public function insertAlertForOne($user_id, $data = array()){		
-	
-		$this->cache->addtolist($this->prefix . '_user' . (int)$user_id, serialize($data), 600);
-		
+	public function insertAlertForOne($user_id, $data = array()){			
+		$this->cache->addtolist($this->prefix . '_user' . (int)$user_id, serialize($data), 600);		
 	}
 	
-	public function getAlert($user_id){
-		
-		return $this->cache->rpoplist($this->prefix . '_user' . (int)$user_id);
-				
+	public function getAlert($user_id){		
+		return $this->cache->rpoplist($this->prefix . '_user' . (int)$user_id);				
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
