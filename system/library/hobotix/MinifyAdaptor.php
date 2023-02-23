93f67e8e500a96f4d533b126434e1567bef4b9df
Victor <v.zaichikov@gmail.com>
2023-02-23 11:13:47 +0200

    Additional file existense check in MinifyAdaptor

--- system/library/hobotix/MinifyAdaptor.php
+++ system/library/hobotix/MinifyAdaptor.php
@@ -72,7 +72,7 @@ final class MinifyAdaptor
 		$times = [];
 		foreach ($input as $file){
 			$file = DIR_ENGINE . ltrim($file, '/');
-			if ($time = filemtime($file)){
+			if (file_exists($file) && $time = filemtime($file)){
 				$files[] 		= $file;
 				$times[$file] 	= $time;	
 			}
