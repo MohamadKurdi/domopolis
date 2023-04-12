34c8020904e2a3d55f34f4bf372ff9e78734db94
Victor <v.zaichikov@gmail.com>
2023-04-12 21:22:20 +0300

    Small bugfix in main order model

--- catalog/model/checkout/order.php
+++ catalog/model/checkout/order.php
@@ -325,8 +325,10 @@ class ModelCheckoutOrder extends Model {
 
 			//Настройка "получать офферы после заказа"
 		if ($this->config->get('config_rainforest_enable_offers_after_order')){
-			$product_query = $this->db->query("SELECT asin FROM product WHERE product_id = '" . (int)$product['product_id'] . "'");
-			$this->db->query("INSERT IGNORE INTO amzn_product_queue SET asin = '" . $this->db->escape($product_query->row['asin']) . "'");				
+			$asin_query = $this->db->query("SELECT asin FROM product WHERE product_id = '" . (int)$product['product_id'] . "'");
+			if ($asin_query->row['asin']){
+				$this->db->query("INSERT IGNORE INTO amzn_product_queue SET asin = '" . $this->db->escape($asin_query->row['asin']) . "', date_added = NOW()");
+			}			
 		}
 
 			//Резервирование товара
