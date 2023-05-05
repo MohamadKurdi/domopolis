ef08f69da377624e107eb823b5fd135a2436be37
Victor <v.zaichikov@gmail.com>
2023-05-05 17:11:24 +0300

    Full refactor of banner module to use slides and grouping

--- catalog/controller/module/banner.php
+++ catalog/controller/module/banner.php
@@ -6,68 +6,53 @@ class ControllerModuleBanner extends Controller {
 		$this->load->model('design/banner');
 		$this->load->model('tool/image');
 
-		$this->data['blocks'] = [];
-		$this->data['blocks_sm'] = [];
-		
-		$results = $this->model_design_banner->getBanner($setting['banner_id']);
+		$this->data['slides'] = [];		
+		$results = $this->model_design_banner->getBanners($setting['banner_id']);
+
+		foreach ($results as $result){
+
+			$this->data['slides'][$result['banner_id']] = [
+				'banner_id' => $result['banner_id'],
+				'class'  	=> $result['class'],
+				'class_sm'  => $result['class_sm'],
+				'images'  	=> []
+			];
 
 
-		foreach ($results as $result) {			
-			if (empty($this->data['blocks'][$result['block']])){
-				$this->data['blocks'][$result['block']] = [];
+			foreach ($result['images'] as $image){
+				if (empty($image['width'])){
+					$image['width'] = $setting['width'];
+				}
+
+				if (empty($image['height'])){
+					$image['height'] = $setting['height'];
+				}
+
+				if (empty($image['width_sm'])){
+					$image['width_sm'] = $setting['width_sm'];
+				}
+
+				if (empty($image['height_sm'])){
+					$image['height_sm'] = $setting['height_sm'];
+				}
+
+
+				if (file_exists(DIR_IMAGE . $image['image'])) {
+					$this->data['slides'][$result['banner_id']]['images'][] = [
+						'title' 	=> $image['title'],
+						'link'  	=> $image['link'],
+						'class' 	=> $image['class'],
+						'width' 	=> $image['width'],
+						'height' 	=> $image['height'],
+						'class_sm' 	=> $image['class_sm'],
+						'width_sm' 	=> $image['width_sm'],
+						'height_sm' => $image['height_sm'],
+						'image'		=> $this->model_tool_image->resize($image['image'], $image['width'], $image['height']),
+						'image_sm'	=> $this->model_tool_image->resize($image['image_sm'], $image['width_sm'], $image['height_sm']),
+					];
+				}
 			}
-
-			if (empty($this->data['blocks_sm'][$result['block_sm']])){
-				$this->data['blocks_sm'][$result['block_sm']] = [];
-			}
-
-			if (empty($result['width'])){
-				$result['width'] = $setting['width'];
-			}
-
-			if (empty($result['height'])){
-				$result['height'] = $setting['height'];
-			}
-
-			if (empty($result['width_sm'])){
-				$result['width_sm'] = $setting['width_sm'];
-			}
-
-			if (empty($result['height_sm'])){
-				$result['height_sm'] = $setting['height_sm'];
-			}
-
-			if (file_exists(DIR_IMAGE . $result['image'])) {
-				$this->data['blocks'][$result['block']][] = [
-					'title' 	=> $result['title'],
-					'link'  	=> $result['link'],
-					'class' 	=> $result['class'],
-					'width' 	=> $result['width'],
-					'height' 	=> $result['height'],
-					'class_sm' 	=> $result['class_sm'],
-					'width_sm' 	=> $result['width_sm'],
-					'height_sm' => $result['height_sm'],
-					'image'		=> $this->model_tool_image->resize($result['image'], $result['width'], $result['height']),
-					'image_sm'	=> $this->model_tool_image->resize($result['image_sm'], $result['width_sm'], $result['height_sm']),
-				];
-			}
-
-			if (file_exists(DIR_IMAGE . $result['image_sm'])) {
-				$this->data['blocks_sm'][$result['block_sm']][] = [
-					'title' 	=> $result['title'],
-					'link'  	=> $result['link'],
-					'class' 	=> $result['class'],
-					'width' 	=> $result['width'],
-					'height' 	=> $result['height'],
-					'class_sm' 	=> $result['class_sm'],
-					'width_sm' 	=> $result['width_sm'],
-					'height_sm' => $result['height_sm'],
-					'image'		=> $this->model_tool_image->resize($result['image'], $result['width'], $result['height']),
-					'image_sm'	=> $this->model_tool_image->resize($result['image_sm'], $result['width_sm'], $result['height_sm']),
-				];
-			}
-		}		
-
+		}
 		
 		$this->data['module'] = $module++;
 		
