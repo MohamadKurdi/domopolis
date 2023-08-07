9761f442ecf6a5843a6510b8ed719d04d3faa6ed
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2023-08-07 15:04:28 +0300

    Minor changes

--- catalog/controller/common/errorgen.php
+++ catalog/controller/common/errorgen.php
@@ -2,6 +2,20 @@
 	
 class ControllerCommonErrorGen extends Controller {
 
+	public function testViber(){
+
+		$this->smsAdaptor->sendSMS(
+			[
+				'to' 		=> '+380632708881',
+				'message' 	=> 'THIS IS CHAIN TEST'
+			]
+		);
+
+
+
+
+	}
+
 	public function index(){
 	
 		$this->log->debug($this->url->link('product/category', 'path=6415' . '&filterinstock=1'));
