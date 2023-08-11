b34769345d1b76360ee713a52475f8f45a65f874
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2023-08-11 16:46:55 +0300

    Added some language strings and removed old unused language files

--- catalog/controller/common/errorgen.php
+++ catalog/controller/common/errorgen.php
@@ -4,14 +4,16 @@ class ControllerCommonErrorGen extends Controller {
 
 	public function testViber(){
 		$this->load->model('checkout/order');
+		$this->load->model('account/customer');
 
-		$order_info = $this->model_checkout_order->getOrder(317671);
-		$data = [
-			'amount' => 200,
-			'order_status_id' => 7
+		$order_info 	= $this->model_checkout_order->getOrder(317671);
+		$customer_info 	= $this->model_account_customer->getCustomer($order_info['customer_id']);
+		$data = [			
+			'points_amount' => 1000,
+			'points_days_left' => 5
 		];
 
-		$this->smsAdaptor->sendPayment($order_info, $data);
+		$this->smsAdaptor->sendRewardReminder($customer_info, $data);
 
 
 	}
