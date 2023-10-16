572c0d7bb1bce5e4d1a5eeb5270c590a4d52b3a3
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2023-10-16 16:15:34 +0300

    Bugfix in mono payment language in admin

--- admin/language/russian/payment/mono.php
+++ admin/language/russian/payment/mono.php
@@ -4,22 +4,25 @@ $_['heading_title']                 = 'Monobank';
 
 // Text
 $_['text_mono']                     = '<a onclick="window.open(\'https://monobank.ua\');"><img src="view/image/payment/monobank.svg" alt="Monobank" title="Monobank"/></a>';
-$_['text_success']                  = 'Настройки модуля обновлены!';
+$_['text_success']                  = 'Налаштування модуля оновлені!';
 $_['text_pay']                      = 'Monobank';
-$_['mono_text']                     = 'Узнать свой X-Token Вы можете по ссылке:';
+$_['mono_text']                     = 'Дізнатися свій X-Token Ви можете за посиланням:';
 
 // Entry
 $_['entry_merchant']                = 'X-Token';
-$_['entry_geo_zone']                = 'Географическая зона';
-$_['entry_order_success_status']    = 'Статус оплаченного заказа';
-$_['entry_order_process_status']    = 'Статус заказа в обработке';
-$_['entry_order_cancelled_status']  = 'Статус отмененного заказа';
+$_['entry_geo_zone']                = 'Географічна зона';
+$_['entry_order_success_status']    = 'Статус сплаченого замовлення';
+$_['entry_order_process_status']    = 'Статус замовлення в обробці';
+$_['entry_order_cancelled_status']  = 'Статус відміненного замовлення';
 $_['entry_status']                  = 'Статус';
-$_['entry_sort_order']              = 'Порядок сортировки';
+$_['entry_sort_order']              = 'Порядок сортування';
 
 // Error
-$_['error_merchant']                = 'Неверный X-Token!';
-$_['error_permission']              = 'Недостаточно прав!';
-
+$_['error_merchant']                = 'Невірний X-Token!';
+$_['error_permission']              = 'Недостатньо прав!';
 //Button
-$_['save_btn']                      = 'Сохранить';
+$_['save_btn']                      = 'Зберегти';
+
+$_['entry_redirect']                = 'Перенаправлення після оплати. Зразок: "index.php?route=information/pay-success"';
+$_['entry_destination']             = 'Призначення платежу';
+$_['entry_hold']                    = 'Режим холдів';
