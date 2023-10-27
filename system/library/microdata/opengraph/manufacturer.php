95d2fcb6c1d5e994d5cde30db5fb5ef73d55760a
Victor <v.zaichikov@gmail.com>
2023-10-27 16:29:05 +0300

    Some prettyprints of microdata libraries

--- system/library/microdata/opengraph/manufacturer.php
+++ system/library/microdata/opengraph/manufacturer.php
@@ -1,1 +1,10 @@
-<? if( $this->config->get( 'opengraph_status' ) == 1 ) { $this->document ->addMeta( 'og:type', 'og:website', 'property' ) ->addMeta( 'og:title', $manufacturer_info['name'], 'property' ) ->addMeta( 'og:url', $this->url->link( 'product/manufacturer/info', 'manufacturer_id=' . $this->request->get['manufacturer_id'] . $url ), 'property' ) ->addMeta( 'og:image', $this->model_tool_image->resize( $manufacturer_info['image'], $this->config->get('config_image_category_width'), $this->config->get('config_image_category_height') ), 'property' ) ->addMeta( 'og:description', str_replace( "\"", "&quot;", utf8_substr(trim(strip_tags( html_entity_decode($manufacturer_info['description'], ENT_QUOTES, 'UTF-8') ), " \t\n\r"), 0, 500) . '..' ), 'property' ) ->addMeta( 'og:site_name', $this->config->get('config_name'), 'property' ); } ?>
+<?php
+ if ( $this->config->get( 'opengraph_status' ) == 1 ) { 
+    $this->document 
+    ->addMeta( 'og:type', 'og:website', 'property' ) 
+    ->addMeta( 'og:title', $manufacturer_info['name'], 'property' ) 
+    ->addMeta( 'og:url', $this->url->link( 'product/manufacturer/info', 'manufacturer_id=' . $this->request->get['manufacturer_id'] . $url ), 'property' ) 
+    ->addMeta( 'og:image', $this->model_tool_image->resize( $manufacturer_info['image'], $this->config->get('config_image_category_width'), $this->config->get('config_image_category_height') ), 'property' ) 
+    ->addMeta( 'og:description', str_replace( "\"", "&quot;", utf8_substr(trim(strip_tags( html_entity_decode($manufacturer_info['description'], ENT_QUOTES, 'UTF-8') ), " \t\n\r"), 0, 500) . '..' ), 'property' ) 
+    ->addMeta( 'og:site_name', $this->config->get('config_name'), 'property' ); 
+}
--- system/library/microdata/opengraph/product.php
+++ system/library/microdata/opengraph/product.php
@@ -1,4 +1,5 @@
-<? if( $this->config->get( 'opengraph_status' ) == 1 ) {
+<?php
+ if( $this->config->get( 'opengraph_status' ) == 1 ) {
 	$this->document ->addMeta( 'og:type', 'product', 'property' ) 
 	->addMeta( 'og:title', $product_info['name'], 'property' ) 
 	->addMeta( 'og:url', $this->url->link( 'product/product', 'product_id=' . $product_info['product_id'] ), 'property' ) 
@@ -6,4 +7,5 @@
 	->addMeta( 'product:price:currency', $this->currency->getCode(), 'property' ) 
 	->addMeta( 'og:image', $this->model_tool_image->resize( $product_info['image'], 600, 600 ), 'property' ) 
 	->addMeta( 'og:description', str_replace( "\"", "&quot;", utf8_substr(trim(strip_tags( html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8') ), " \t\n\r"), 0, 500) . '..' ), 'property' ) 
-	->addMeta( 'og:site_name', $this->config->get('config_name'), 'property' ); } ?>
+	->addMeta( 'og:site_name', $this->config->get('config_name'), 'property' ); 
+}
