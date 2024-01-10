0548d48db5b0906eb1f7d265ae2597e6c061111a
Victor <v.zaichikov@gmail.com>
2024-01-10 14:41:23 +0200

    Fixed bug in response lib while checking output

--- system/library/response.php
+++ system/library/response.php
@@ -307,7 +307,7 @@ class Response {
    * Output response
    */
   public function output() {
-  	if ($this->output) {								
+  	if (mb_strlen($this->output)) {								
   		if (!headers_sent()) {
   			foreach ($this->headers as $header) {
   				header($header, true);
