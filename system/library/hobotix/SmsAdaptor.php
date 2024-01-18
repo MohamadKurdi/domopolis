ba6604a86d92c0e333c1538468b4e9232da48328
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2024-01-18 13:18:00 +0200

    Added forgotten cart sending logic to SmsAdaptor (initial)

--- system/library/hobotix/SmsAdaptor.php
+++ system/library/hobotix/SmsAdaptor.php
@@ -312,6 +312,59 @@ class SmsAdaptor {
 		}
 	}
 
+	public function sendSMSForgottenCartPromo($forgotten_cart, $data){
+		$iteration = $data['iteration_id'];
+
+		$template = [
+			'{ID}' 				=> $forgotten_cart['order_id'], 
+			'{SNAME}'			=> $this->config->get('config_name'), 
+			'{DATE}'			=> date('d.m.Y'), 
+			'{TIME}'			=> date('H:i:s'), 
+			'{PHONE}'			=> $forgotten_cart['telephone'], 
+			'{FIRSTNAME}'		=> $forgotten_cart['firstname'], 
+			'{LASTNAME}' 		=> $forgotten_cart['lastname'],
+			'{PRODUCT_TEXT}' 	=> $forgotten_cart['product_text'],
+			'{PROMOCODE}'   	=> $this->config->get('config_firstorder_promocode')
+		];
+
+		if ($this->config->get('config_viber_forgottencart_enabled_' . $iteration)){
+			$viber = [
+				'viber' 		=> true,
+				'to' 			=> $forgotten_cart['telephone'],
+				'message' 		=> reTemplate($template, $this->config->get('config_viber_forgottencart_' . $iteration)),
+				'messageSms' 	=> reTemplate($template, $this->config->get('config_firstorder_sms_text')),
+
+				'button_txt' 	=> $this->config->get('config_viber_forgottencart_button_text_' . $iteration),
+				'button_url' 	=> $this->config->get('config_viber_firstorder_button_' . $iteration), 				
+			];
+
+			if (!empty($forgotten_cart['product_image'])){
+				$viber['picture_url'] = $forgotten_cart['product_image'];
+			}
+
+			if (!empty($this->config->get('config_viber_forgottencart_image_' . $iteration)) && file_exists(DIR_IMAGE . $this->config->get('config_viber_forgottencart_image_' . $iteration))){
+				$viber['picture_url'] = HTTPS_CATALOG . DIR_IMAGE_NAME . $this->config->get('config_viber_forgottencart_image_' . $iteration);
+			}
+
+			$viberID = $this->registry->get('smsQueue')->queue($viber);
+			$this->addOrderSmsHistory(false, ['sms' => $viber['message']], 'Queued', $viberID, (int)$forgotten_cart['customer_id']);
+
+			return $viberID;
+		}
+
+		if ($this->config->get('config_forgottencart_sms_enable_' . $iteration)){
+			$sms = [
+				'to' 		=> $forgotten_cart['telephone'],
+				'message' 	=> reTemplate($template, $this->config->get('config_forgottencart_sms_text_' . $iteration))
+			];
+
+			$smsID = $this->registry->get('smsQueue')->queue($sms);
+			$this->addOrderSmsHistory(false, ['sms' => $sms['message']], 'Queued', $smsID, (int)$forgotten_cart['customer_id']);
+
+			return $smsID;
+		}
+	}
+
 
 	public function sendSMSFirstOrderPromo($order_info, $data){
 		$template = [
