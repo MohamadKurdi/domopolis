ec230904f85f151c0748039e10d9dedb48d798d1
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2024-02-13 11:26:43 +0200

    Implemented new supplier adaptor

--- system/library/hobotix/Supplier/Adaptors/StoreoUAXMLv1.php
+++ system/library/hobotix/Supplier/Adaptors/StoreoUAXMLv1.php
@@ -26,6 +26,7 @@ class StoreoUAXMLv1 extends SuppliersGeneralClass {
 							'category_id' 		=> $item['@attributes']['id'],
 							'parent_id' 		=> '',
 							'category_name' 	=> $item['@value'],
+							'category_name_full'=> $item['@value'],
 						];					
 					}
 				}
@@ -63,5 +64,98 @@ class StoreoUAXMLv1 extends SuppliersGeneralClass {
 		return $product_attributes;
 	}
 
+
+	public function getProducts(){
+		$products = [];
+
+		if (!$this->getContent()){
+			$this->setContent();
+		}
+
+		$this->setCategories();
+
+		if (!empty($this->content['yml_catalog'])){
+			if (!empty($this->content['yml_catalog']['shop'])){
+				if (!empty($this->content['yml_catalog']['shop']['offers'])){
+					if (!empty($this->content['yml_catalog']['shop']['offers']['offer'])){
+						foreach ($this->content['yml_catalog']['shop']['offers']['offer'] as $offer){
+
+							$name = [];
+							if (!empty($offer['name_ua'])){
+								$name['uk'] = checkCDATA($offer['name_ua']);
+							}
+							if (!empty($offer['name'])){
+								$name['ru'] = ['translate_from' => 'uk', 'translate_data' => checkCDATA($offer['name'])];
+							}
+
+							$description = [];
+							if (!empty($offer['description_ua'])){
+								$description['uk'] = checkCDATA($offer['description_ua']);
+							}
+							if (!empty($offer['description'])){
+								$description['ru'] = ['translate_from' => 'uk', 'translate_data' => checkCDATA($offer['description'])];
+							}
+
+							$image 	= '';	
+							$images = [];
+
+							$pictures 	= checkSingleXMLItem($offer['picture']);							
+							$image 		= $pictures[0];
+
+							foreach ($pictures as $picture){
+								$images[] = $picture;
+							}
+
+							if (!empty($images[0])){
+								unset($images[0]);
+							}
+
+							$product_attributes = [];
+							if (!empty($offer['param'])){
+								$params = checkSingleXMLItemByField($offer['param'], '@attributes');
+
+								foreach ($params as $param){
+									$product_attributes[mb_ucfirst(checkCDATA($param['@attributes']['name']))] = [
+										'name' 	=> [
+											'uk' => mb_ucfirst(checkCDATA($param['@attributes']['name'])),
+											'ru' => ['translate_from' => 'uk', 'translate_data' => mb_ucfirst(checkCDATA($param['@attributes']['name']))]
+										],
+										'text' => [
+											'uk' => mb_ucfirst(checkCDATA($param['@value'])),
+											'ru' => ['translate_from' => 'uk', 'translate_data' => mb_ucfirst(checkCDATA($param['@value']))]
+										]
+									];
+								}
+							}
+
+							$products[] = [
+								'supplier_product_id' 	=> $offer['@attributes']['id'],
+								'status'				=> (bool)$offer['@attributes']['available'],
+								'name'					=> $name,
+								'description' 			=> $description,
+								'model' 				=> $offer['vendorCode'],
+								'sku' 					=> $offer['vendorCode'],
+								'ean' 					=> $offer['EAN'],
+								'image' 				=> $image,
+								'images' 				=> $images,
+								'stock' 				=> ((bool)$offer['@attributes']['in_stock'])?true:false,
+								'quantity' 				=> (int)$offer['quantity_in_stock'],
+								'price' 				=> (float)$offer['price'],						
+								'vendor' 				=> $offer['vendor'],
+								'vendor_country'		=> $offer['country'],
+								'category'  			=> $offer['categoryId'],
+								'attributes'            => $product_attributes,
+								'raw' 					=> json_encode($offer)
+							];
+
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		return $products;
+	}
 
 }
