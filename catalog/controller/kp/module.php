35fd50dcd7daae3f4fcf87ca2f8578ae497a8f28
Victor <v.zaichikov@gmail.com>
2024-02-23 15:09:46 +0200

    Moved response to json in ajax module api

--- catalog/controller/kp/module.php
+++ catalog/controller/kp/module.php
@@ -1,51 +1,42 @@
 <?php
-	
-	
-	class ControllerKPModule extends Controller {
-		
-		public function index(){
-			
-			if (empty($this->request->get['modpath'])){
-				$this->response->setOutput('');
-				} else {
-				$this->data = [];
-				if (!empty($this->request->get['group'])){
-					$exploded = explode(';', $this->request->get['modpath']);
-					foreach ($exploded as $line){
-						if (trim($line)){
-							
-							try{
-								
-								$this->data[] = [
+
+
+class ControllerKPModule extends Controller {
+
+	public function index(){
+
+		if (empty($this->request->get['modpath'])){
+			$this->response->setOutput('');
+		} else {
+			$this->data = [];
+			if (!empty($this->request->get['group'])){
+				$exploded = explode(';', $this->request->get['modpath']);
+				foreach ($exploded as $line){
+					if (trim($line)){
+						try{
+
+							$this->data[] = [
 								'path' 	=> $line,
 								'html'	=> $this->getChild($line)
-								];
-								
-								} catch (Exception $e){
-							}
-							
-						}
-					}		
-					
-					$this->response->setOutput(json_encode($this->data));
-					
-					} else {
-					
-					
-					
-					try{				
-						$this->data['data'] = $this->getChild($this->request->get['modpath']);
+							];
+
 						} catch (Exception $e){
-						$this->data['data'] = '';
+						}
 					}
-					
-					$this->template = $this->config->get('config_template') . '/template/structured/module.tpl';
-					$this->response->setOutput($this->render());
-					
+				}		
+
+				$this->response->setJSON($this->data);
+
+			} else {
+				try{				
+					$this->data['data'] = $this->getChild($this->request->get['modpath']);
+				} catch (Exception $e){
+					$this->data['data'] = '';
 				}
-				
-			}
+
+				$this->template = $this->config->get('config_template') . '/template/structured/module.tpl';
+				$this->response->setOutput($this->render());					
+			}				
 		}
-		
-		
-	}
+	}				
+}
