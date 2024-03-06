816efea84b9eff700b4d3cfa773b8267f86448c9
VICTOR HOBOTOV <v.zaichikov@gmail.com>
2024-03-06 12:27:04 +0200

    Bugfixes in admin total module

--- admin/controller/total/totalukrcredits.php
+++ admin/controller/total/totalukrcredits.php
@@ -2,28 +2,21 @@
 class ControllerTotalTotalukrcredits extends Controller {
 	private $error = array();
 
-	public function index() {
-		
-		$token = version_compare(VERSION,'3.0','>=') ? 'user_' : '';
-		$type = version_compare(VERSION,'3.0','>=') ? 'total_' : '';
-		$dir = version_compare(VERSION,'2.3','>=') ? 'extension/total' : 'total';
-		$total_page = version_compare(VERSION,'3.0','>=') ? 'marketplace/extension' :
-			(version_compare(VERSION,'2.3','>=') ? 'extension/extension' : 'extension/total');
-			
+	public function index() {			
 		$data['type'] = $type;
 			
-		$this->load->language($dir.'/totalukrcredits');
+		$this->load->language('total/totalukrcredits');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->load->model('setting/setting');
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting($type.'totalukrcredits', $this->request->post);
+			$this->model_setting_setting->editSetting('totalukrcredits', $this->request->post);
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$this->response->redirect($this->url->link($total_page, $token.'token=' . $this->session->data[$token.'token'] . '&type=total', true));
+			$this->response->redirect($this->url->link('extension/total', $token.'token=' . $this->session->data['token'] . '&type=total', true));
 		}
 
 		$data['heading_title'] = $this->language->get('heading_title');
@@ -48,56 +41,39 @@ class ControllerTotalTotalukrcredits extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', $token.'token=' . $this->session->data[$token.'token'], true)
+			'href' => $this->url->link('common/dashboard', $token.'token=' . $this->session->data['token'], true)
 		);
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_total'),
-			'href' => $this->url->link($total_page, $token.'token=' . $this->session->data[$token.'token'] . '&type=total', true)
+			'href' => $this->url->link('extension/total', $token.'token=' . $this->session->data['token'] . '&type=total', true)
 		);
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link($dir.'/totalukrcredits', $token.'token=' . $this->session->data[$token.'token'], true)
+			'href' => $this->url->link('total/totalukrcredits', $token.'token=' . $this->session->data['token'], true)
 		);
 
-		$data['action'] = $this->url->link($dir.'/totalukrcredits', $token.'token=' . $this->session->data[$token.'token'], true);
-		$data['cancel'] = $this->url->link($total_page, $token.'token=' . $this->session->data[$token.'token'] . '&type=total', true);
+		$data['action'] = $this->url->link('total/totalukrcredits', $token.'token=' . $this->session->data['token'], true);
+		$data['cancel'] = $this->url->link('extension/total', $token.'token=' . $this->session->data['token'] . '&type=total', true);
 
-		if (isset($this->request->post[$type.'totalukrcredits_status'])) {
-			$data[$type.'totalukrcredits_status'] = $this->request->post[$type.'totalukrcredits_status'];
+		if (isset($this->request->post['totalukrcredits_status'])) {
+			$data['totalukrcredits_status'] = $this->request->post['totalukrcredits_status'];
 		} else {
-			$data['totalukrcredits_status'] = $this->config->get($type.'totalukrcredits_status');
+			$data['totalukrcredits_status'] = $this->config->get('totalukrcredits_status');
 		}
 
-		if (isset($this->request->post[$type.'totalukrcredits_sort_order'])) {
-			$data[$type.'totalukrcredits_sort_order'] = $this->request->post[$type.'totalukrcredits_sort_order'];
+		if (isset($this->request->post['totalukrcredits_sort_order'])) {
+			$data['totalukrcredits_sort_order'] = $this->request->post['totalukrcredits_sort_order'];
 		} else {
-			$data['totalukrcredits_sort_order'] = $this->config->get($type.'totalukrcredits_sort_order');
+			$data['totalukrcredits_sort_order'] = $this->config->get('totalukrcredits_sort_order');
 		}
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 		
-		if (version_compare(VERSION, '3.0.0', '>=')) {
-			$template_engine = $this->registry->get('config')->get('template_engine');
-			$template_directory = $this->registry->get('config')->get('template_directory');
-			$this->registry->get('config')->set('template_engine', 'template');
-			if (!file_exists(DIR_TEMPLATE . $template_directory . 'total/totalukrcredits' . '.tpl')) {
-				$this->registry->get('config')->set('template_directory', 'default/template/');
-			}
-			$template = $this->load->view('total/totalukrcredits', $data);
-			
-			$this->registry->get('config')->set('template_engine', $template_engine);
-			$this->registry->get('config')->set('template_directory', $template_directory);
-			
-			$this->response->setOutput($template);
-		} else if (version_compare(VERSION,'2.3','>=')) {
-			$this->response->setOutput($this->load->view('total/totalukrcredits', $data));
-		} else {
-			$this->response->setOutput($this->load->view('total/totalukrcredits.tpl', $data));
-		}
+		$this->response->setOutput($this->load->view('total/totalukrcredits.tpl', $data));
 	}
 
 	protected function validate() {
