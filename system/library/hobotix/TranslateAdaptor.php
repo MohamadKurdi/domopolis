e3a4c2ccfc497dbde626f501523109e5d8da705f
Victor Zaichikov <v.zaichikov@gmail.com>
2024-03-13 17:02:45 +0200

    Implemented translation strings cache logic to TranslateAdaptor

--- system/library/hobotix/TranslateAdaptor.php
+++ system/library/hobotix/TranslateAdaptor.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 namespace hobotix;
 
@@ -8,12 +8,20 @@ class TranslateAdaptor {
 	private $db  				= null;
 	private $translateObject 	= null;
 
+    private const cacheStringLengthLimit = 255;
+
 	public function __construct($registry) {		
 		$this->registry = $registry;
 		$this->config 	= $registry->get('config');
 		$this->db 		= $registry->get('db');
-		
-		$this->use($this->config->get('config_translation_library'));
+
+        if ($this->config->get('config_enable_translation_cache')){
+            echoLine('[TranslateAdaptor::__construct] Translation cache is enabled!', 's');
+        }
+
+        if (!$this->translateObject){
+            $this->use($this->config->get('config_translation_library'));
+        }
 	}
 
 	public function getTranslateLibraries(){
@@ -33,7 +41,7 @@ class TranslateAdaptor {
 			$translateClass = "hobotix" . "\\" . "Translate" . "\\" . $translateClass;
 			$this->translateObject = new $translateClass($this->registry);
 
-			echoLine('[TranslateAdaptor::use] Using ' . $translateClass . ' translation library', 's');			
+			echoLine('[TranslateAdaptor::use] Using ' . $translateClass . ' translation library', 's');
 		} else {
 			echoLine('[TranslateAdaptor::use] Tried to use ' . $translateClass . ' translation library, but failed', 'e');	
 		}
@@ -45,6 +53,8 @@ class TranslateAdaptor {
 		if (method_exists($this->translateObject, 'setDebug')){
 			return $this->translateObject->setDebug($debug);
 		}
+
+        return $this;
 	}
 
 	public function checkIfItIsPossibleToMakeRequest(){
@@ -66,16 +76,66 @@ class TranslateAdaptor {
 		return $result;
 	}
 
+    private function updateCacheHitUsage($translation_cache_id){
+        $this->db->query("UPDATE translation_cache SET `usages` = `usages` + 1 WHERE translation_cache_id = '" . (int)$translation_cache_id . "'");
+    }
+
+    private function setTranslationStringCache($from, $to, $text, $translation){
+        if (mb_strlen($text) >= self::cacheStringLengthLimit){
+            return false;
+        }
+
+        $this->db->query("INSERT INTO translation_cache SET 
+            language_code_from = '" . $this->db->escape($from) . "',
+            language_code_to = '" . $this->db->escape($to) . "',
+            string = '" . $this->db->escape($text) . "',
+            translation = '" . $this->db->escape($translation) . "'
+            ON DUPLICATE KEY UPDATE
+            translation = '" . $this->db->escape($translation) . "'");
+    }
+
+    private function getTranslationStringFromCache($from, $to, $text){
+        if (mb_strlen($text) >= self::cacheStringLengthLimit){
+            return false;
+        }
+
+        $query = $this->db->ncquery("SELECT translation_cache_id, translation FROM translation_cache WHERE 
+            language_code_from = '" . $this->db->escape($from) . "'
+            AND language_code_to = '" . $this->db->escape($to) . "'
+            AND string = '" . $this->db->escape($text) . "'");
+
+        if ($query->num_rows){
+            echoLine('[TranslateAdaptor::cache] Got translation string from cache: ' . $query->row['translation'], 's');
+
+            $this->updateCacheHitUsage($query->row['translation_cache_id']);
+            return $query->row['translation'];
+        }
+
+        return false;
+    }
 
 	public function translate($text, $from, $to, $returnString = false){
 		if (method_exists($this->translateObject, 'translate')){
+            $translation = false;
+
 			try {
-				$result = $this->translateObject->translate($text, $from, $to, $returnString);
+                if ($returnString && $this->config->get('config_enable_translation_cache')){
+                    $translation = $this->getTranslationStringFromCache($from, $to, $text);
+
+                    if ($translation){
+                        return $translation;
+                    } else {
+                        $translation = $this->translateObject->translate($text, $from, $to, $returnString);
+                        $this->setTranslationStringCache($from, $to, $text, $translation);
+                    }
+                } else {
+                    $translation = $this->translateObject->translate($text, $from, $to, $returnString);
+                }
 			} catch (\Exception $e){
 				throw new \Exception ($e->getMessage());
 			}
 		}
 
-		return $result;
+		return $translation;
 	}
 }
