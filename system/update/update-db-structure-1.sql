5120082dec1a92f6507002a8ffdb2e0a81a5c4e1
Victor Zaichikov <v.zaichikov@gmail.com>
2024-03-22 14:41:00 +0200

    Updated inner version and db update file

--- system/update/update-db-structure-1.sql
+++ system/update/update-db-structure-1.sql
@@ -1,3 +1,9 @@
-ALTER TABLE `suppliers` ADD `skip_no_manufacturer` BOOLEAN NOT NULL DEFAULT FALSE AFTER `skip_no_category`;
-ALTER TABLE `supplier_manufacturers` ADD UNIQUE(`supplier_id`, `manufacturer`);
-ALTER TABLE `supplier_manufacturers` ADD `manufacturer_full` VARCHAR(256) NOT NULL AFTER `manufacturer`;
+CREATE TABLE `attribute_variants` (`attribute_id` INT(11) NOT NULL , `attribute_variant` VARCHAR(256) NOT NULL , INDEX (`attribute_id`)) ENGINE = InnoDB;
+ALTER TABLE `attribute_variants` ADD `attribute_variant_id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`attribute_variant_id`);
+ALTER TABLE `attribute_variants` ADD `language_id` INT NOT NULL AFTER `attribute_id`, ADD INDEX (`language_id`);
+ALTER TABLE `attribute_variants` ADD INDEX(`attribute_variant`);
+CREATE TABLE `attributes_required_category` (`attribute_id` INT NOT NULL , `category_id` INT NOT NULL , INDEX (`category_id`), INDEX (`attribute_id`)) ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `barbara_blog_category`, `barbara_blog_category_description`, `barbara_blog_category_filter`, `barbara_blog_category_path`, `barbara_blog_category_to_layout`,
+`barbara_blog_category_to_store`, `barbara_blog_post`, `barbara_blog_postmeta`, `barbara_blog_post_description`, `barbara_blog_post_filter`, `barbara_blog_post_to_category`,
+`barbara_blog_post_to_layout`, `barbara_blog_post_to_store`, `barbara_blog_related_product`, `barbara_blog_setting`, `barbara_blog_setting_general`, `barbara_singleclick`, `barbara_stickers`;
